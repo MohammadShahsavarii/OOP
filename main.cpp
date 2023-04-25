5117d7658d7559a16e5eeaf132ee9b2d46601b2a
MohammadShahsavarii <shahsavari.mo@gmail.com>
2023-04-25 23:17:41 +0300

    commit message

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-class door
-{
+class Door {
 private:
     bool isOpen;
 
@@ -14,51 +12,95 @@ public:
     bool isOpened();
 };
 
+void Door::open() {
+    isOpen = true;
+}
 
+void Door::close() {
+    isOpen = false;
+}
 
-class lamp
-{
+bool Door::isOpened() {
+    return isOpen;
+}
+
+class Lamp {
 private:
-    int brithness;
+    int brightness;
     bool isOpen;
 
 public:
     void on();
     void off();
-    void brightness(int brightness);
+    void setBrightness(int brightness);
 };
 
+void Lamp::on() {
+    isOpen = true;
+}
 
+void Lamp::off() {
+    isOpen = false;
+}
 
-class HVAC
-{
+void Lamp::setBrightness(int brightness) {
+    this->brightness = brightness;
+}
+
+class HVAC {
 private:
     int temperature;
     bool isOn;
 
 public:
-    void turnon();
-    void turnoff();
-    void settemperature(int temperature());
+    void turnOn();
+    void turnOff();
+    void setTemperature(int temperature);
 };
 
+void HVAC::turnOn() {
+    isOn = true;
+}
 
+void HVAC::turnOff() {
+    isOn = false;
+}
 
-class tv
-{
+void HVAC::setTemperature(int temperature) {
+    this->temperature = temperature;
+}
+
+class TV {
 private:
     bool isOn;
     int channel;
     int volume;
 
 public:
-    void turnon();
-    void turnoff();
-    void changechannel(int channel);
-    void changevolume(int volume);
+    void turnOn();
+    void turnOff();
+    void changeChannel(int channel);
+    void changeVolume(int volume);
 };
 
-int main()
-{
-
+void TV::turnOn() {
+    isOn = true;
 }
+
+void TV::turnOff() {
+    isOn = false;
+}
+
+void TV::changeChannel(int channel) {
+    this->channel = channel;
+}
+
+void TV::changeVolume(int volume) {
+    this->volume = volume;
+}
+
+int main() {
+    // your code here
+    return 0;
+}
+
